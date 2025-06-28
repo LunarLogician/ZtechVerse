bfdd8dedac682679c9e5291238b931c53dc85f5b
zubair <zzubairahmed402@gmail.com>
2025-06-28 18:05:08 +0500

    Sitemap fix

---
+++ app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://zverse.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://zverse.vercel.app/#about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://zverse.vercel.app/#experience',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://zverse.vercel.app/#projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://zverse.vercel.app/#certifications',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://zverse.vercel.app/#skills',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
